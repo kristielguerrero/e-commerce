80235d3650cb7e2f69ef1931a6c895afde3ac11c
Kris Tina <kristyguerrero20@gmail.com>
2021-08-06 22:53:29 -0500

    created the API routes in the product-routes file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -27,6 +27,20 @@ router.get("/", async (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const data = await Product.findOne({
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -105,6 +119,20 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  try {
+    const data = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!Data) {
+      res.status(404).json({ message: "No product found with this id " });
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
